b5a5565684756d26e0104e4ba2d0fd59923fdb78
Hebert Douglas <hebertdougl@gmail.com>
2015-01-22 15:01:48 -0200

    Alterações na página principal.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :principals
+
   resources :marca_veiculos
 
   resources :ordem_servicos
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'ordem_servicos#index'
+  root 'principals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
